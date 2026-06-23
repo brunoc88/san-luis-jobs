dcc0da4d01379df1fa571f3087cb4479fae5838b
brunoc88 <brunocerutti88@gmail.com>
2026-06-23 13:22:06 -0300

    Update login.schema.ts

--- my-app/src/lib/schemas/login.schema.ts
+++ my-app/src/lib/schemas/login.schema.ts
@@ -3,11 +3,12 @@ import { z } from "zod"
 const LoginSchema = z.object({
   user: z
     .string()
-    .nonempty("Debe ingresar email o nombre de usuario")
-    .transform(val => val.trim().toLowerCase()),
+    .trim()
+    .nonempty("Debe ingresar email o nombre de usuario"),
 
   password: z
     .string()
+    .trim()
     .nonempty("Debe ingresar un password")
     .min(8, "Min 8 caracteres")
 })
